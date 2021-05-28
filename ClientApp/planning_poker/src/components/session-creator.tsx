609c0cf873cf5006fb0df5d337b5d0ede02a5345
Vyacheslav Lopaev <vlopaev@knoema.com>
2021-05-28 19:50:41 +0500

    add busy wrapper

--- ClientApp/planning_poker/src/components/session-creator.tsx
+++ ClientApp/planning_poker/src/components/session-creator.tsx
@@ -2,35 +2,43 @@
 import styles from "../styles/login-form.module.scss";
 import {Button, TextField} from "@material-ui/core";
 import {createSession} from "../models/Api";
+import {useHistory} from "react-router-dom";
+import {BusyWrapper} from "./busy-wrapper";
 
 export const SessionCreator = () => {
 
-    const [title, setTitle ] = useState("");
-    
+    const [title, setTitle] = useState("");
+    const [isBusy, setBusy] = useState(false)
+    const history = useHistory();
+
     async function create() {
+        setBusy(true);
         const session = await createSession(title);
-        console.log(JSON.stringify(session));
+        setBusy(false);
+        history.push(`/session/${session.id}`)
     }
-    
-    return ( 
-        
+
+    return (
+
         <form
-        className={styles.form}
-        noValidate autoComplete="off">
-        <div>
-            <TextField
-                className={styles.username}
-                id="outlined-basic"
-                label="Title"
-                variant="outlined"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-            />
-        </div>
-        <div className={styles.login}>
-            <Button variant="contained" color="primary" onClick={() => create()}>
-                Create
-            </Button>
-        </div>
-    </form>)
+            className={styles.form}
+            noValidate autoComplete="off">
+            <div>
+                <TextField
+                    className={styles.username}
+                    id="outlined-basic"
+                    label="Title"
+                    variant="outlined"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+            </div>
+            <div className={styles.login}>
+                <BusyWrapper Component={
+                    <Button variant="contained" color="primary" onClick={() => create()}>
+                        Create
+                    </Button>
+                }/>
+            </div>
+        </form>)
 }
