219f8de6876c12e1a19f6477b347039754090112
Vyacheslav <mr.slava27@gmail.com>
2021-06-23 01:14:03 +0500

    add some code

---
+++ bot/telegrambot/src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+object Main extends App {
+    val bot = new Bot("")
+    val eol = bot.run()
+    println("Press [ENTER] to shutdown the bot, it may take a few seconds...")
+    scala.io.StdIn.readLine()
+    bot.shutdown() // initiate shutdown
+}
