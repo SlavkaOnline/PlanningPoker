1d81316a394c73c543061162604fb56603daa10d
SlavkaOnline <mr.slava27@gmail.com>
2021-07-29 16:13:10 +0500

    To add ability to configurations game cards when create story

---
+++ WebApi/Application/CardsType.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WebApi.Application
+{
+	public class CardsType
+	{
+		public string Id { get; set; }
+		public string Caption { get; set; }
+		public string[] Cards { get; set; }
+	}
+}
