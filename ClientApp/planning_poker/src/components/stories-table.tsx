c25b3b789a07fecd6ce9b37f9ab2117dfe72e14f
SlavkaOnline <mr.slava27@gmail.com>
2021-09-09 19:24:27 +0500

    refactoring

--- ClientApp/planning_poker/src/components/stories-table.tsx
+++ ClientApp/planning_poker/src/components/stories-table.tsx
@@ -2,25 +2,22 @@ import React, { useEffect, useState } from 'react';
 import { getStory, setActiveStory } from '../models/Api';
 import { Story } from '../models/models';
 import styles from '../styles/stories-table.module.scss';
-import {
-    Button,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Typography,
-} from '@material-ui/core';
+import { Tooltip, Typography } from '@material-ui/core';
 import { OwnerWrapper } from './owner-wrapper';
 import { useSession } from '../contexts/session-context';
 import { useStory } from '../contexts/story-context';
+
+import StopIcon from '@material-ui/icons/Stop';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PauseIcon from '@material-ui/icons/Pause';
+import DoneIcon from '@material-ui/icons/Done';
+import { useAuth } from '../contexts/auth-context';
 
 export const StoriesTable = () => {
     const { session, dispatch } = useSession();
     const [stories, setStories] = useState<readonly Story[]>([]);
     const { story } = useStory();
+    const { user } = useAuth();
 
     useEffect(() => {
         if (session.stories.length) {
@@ -39,60 +36,80 @@ export const StoriesTable = () => {
     }, [story]);
 
     function selectStory(id: string) {
-        setActiveStory(session.id, id).then((s) => dispatch({ tag: 'init', session: s }));
+        if (session.ownerId === user?.id) {
+            setActiveStory(session.id, id).then((s) => dispatch({ tag: 'init', session: s }));
+        }
     }
 
     return (
         <div className={styles.wrapper}>
             <Typography variant="h6">Stories</Typography>
-            <TableContainer component={Paper} className={styles.table}>
-                <Table size="small" aria-label="a dense table" stickyHeader>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell align="right">Status</TableCell>
-                            <TableCell align="right">Result</TableCell>
-                            <OwnerWrapper component={<TableCell align="right">Actions &nbsp;</TableCell>} />
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {stories.map((story) => (
-                            <TableRow className={styles.row} key={story.id}>
-                                <TableCell component="th" scope="row">
-                                    {story.title}
-                                </TableCell>
-                                <TableCell align="right">
-                                    {story.isClosed
-                                        ? 'Finished'
-                                        : story.id === session.activeStory
-                                        ? 'Selected'
-                                        : story.voted.length === 0
-                                        ? 'Not started'
-                                        : 'In progress'}
-                                </TableCell>
-                                <TableCell align="right">{story.result ? story.result : ''}</TableCell>
-                                <OwnerWrapper
-                                    component={
-                                        <TableCell align="right">
-                                            {story.id !== session.activeStory ? (
-                                                <Button
-                                                    onClick={() => selectStory(story.id)}
-                                                    variant="contained"
-                                                    color="default"
-                                                >
-                                                    Select
-                                                </Button>
-                                            ) : (
-                                                <></>
-                                            )}
-                                        </TableCell>
-                                    }
-                                />
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            <div className={styles.table}>
+                {stories.map((story, i) => (
+                    <div key={story.id} className={styles.row} onClick={() => selectStory(story.id)}>
+                        <div className={styles.progress}>
+                            {story.isClosed ? (
+                                <DoneIcon />
+                            ) : story.id === session.activeStory ? (
+                                <PlayArrowIcon />
+                            ) : story.voted.length === 0 ? (
+                                <StopIcon />
+                            ) : (
+                                <PauseIcon />
+                            )}
+                        </div>
+                        <div className={styles.name}> {story.title} </div>
+                    </div>
+                ))}
+            </div>
+            {/*<TableContainer component={Paper} className={styles.table}>*/}
+            {/*    <Table size="small" aria-label="a dense table" stickyHeader>*/}
+            {/*        <TableHead>*/}
+            {/*            <TableRow>*/}
+            {/*                <TableCell>Name</TableCell>*/}
+            {/*                <TableCell align="right">Status</TableCell>*/}
+            {/*                <TableCell align="right">Result</TableCell>*/}
+            {/*                <OwnerWrapper component={<TableCell align="right">Actions &nbsp;</TableCell>} />*/}
+            {/*            </TableRow>*/}
+            {/*        </TableHead>*/}
+            {/*        <TableBody>*/}
+            {/*            {stories.map((story) => (*/}
+            {/*                <TableRow className={styles.row} key={story.id}>*/}
+            {/*                    <TableCell component="th" scope="row">*/}
+            {/*                        {story.title}*/}
+            {/*                    </TableCell>*/}
+            {/*                    <TableCell align="right">*/}
+            {/*                        {story.isClosed*/}
+            {/*                            ? 'Finished'*/}
+            {/*                            : story.id === session.activeStory*/}
+            {/*                            ? 'Selected'*/}
+            {/*                            : story.voted.length === 0*/}
+            {/*                            ? 'Not started'*/}
+            {/*                            : 'In progress'}*/}
+            {/*                    </TableCell>*/}
+            {/*                    <TableCell align="right">{story.result ? story.result : ''}</TableCell>*/}
+            {/*                    <OwnerWrapper*/}
+            {/*                        component={*/}
+            {/*                            <TableCell align="right">*/}
+            {/*                                {story.id !== session.activeStory ? (*/}
+            {/*                                    <Button*/}
+            {/*                                        onClick={() => selectStory(story.id)}*/}
+            {/*                                        variant="contained"*/}
+            {/*                                        color="default"*/}
+            {/*                                    >*/}
+            {/*                                        Select*/}
+            {/*                                    </Button>*/}
+            {/*                                ) : (*/}
+            {/*                                    <></>*/}
+            {/*                                )}*/}
+            {/*                            </TableCell>*/}
+            {/*                        }*/}
+            {/*                    />*/}
+            {/*                </TableRow>*/}
+            {/*            ))}*/}
+            {/*        </TableBody>*/}
+            {/*    </Table>*/}
+            {/*</TableContainer>*/}
         </div>
     );
 };
