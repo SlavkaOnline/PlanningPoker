4a45240da396f1cdaf14c63aafb2222e0c36438f
SlavkaOnline <mr.slava27@gmail.com>
2021-12-05 02:26:56 +0500

    refactoring Integration tests and improving time execution

--- IntegrationTests/ChatTests.fs
+++ IntegrationTests/ChatTests.fs
@@ -26,16 +26,22 @@ type ChatTests(fixture: WebApplicationFactory<Program>) =
 
             let userName1 = "User1"
             let userName2 = "User2"
-            let group = Guid.NewGuid();
+            let group = Guid.NewGuid()
             let message = "simple message"
-            
+
             let! user1 = Helper.login apiClient userName1
             let! user2 = Helper.login apiClient userName1
 
             let tokenHandler = JwtSecurityTokenHandler()
             let token = tokenHandler.ReadJwtToken(user1.Token)
-            let user1Id =  token.Claims |> Seq.filter(fun c -> c.Type = "nameid") |> Seq.tryHead |> Option.map(fun c -> c.Value |> Guid.Parse ) |> Option.defaultValue Guid.Empty
-            
+
+            let user1Id =
+                token.Claims
+                |> Seq.filter (fun c -> c.Type = "nameid")
+                |> Seq.tryHead
+                |> Option.map (fun c -> c.Value |> Guid.Parse)
+                |> Option.defaultValue Guid.Empty
+
             let! userConnection1 = Helper.createEventsConnection server user1.Token
             let! userConnection2 = Helper.createEventsConnection server user2.Token
 
@@ -44,29 +50,29 @@ type ChatTests(fixture: WebApplicationFactory<Program>) =
             do! Task.Delay(pause)
             do! userConnection1.SendAsync("SendMessage", group, message)
 
-            let subs = async {
-                                 let! arr = stream.ReadAllAsync()
-                                             |> AsyncSeq.ofAsyncEnum
-                                             |> AsyncSeq.take(1)
-                                             |> AsyncSeq.toArrayAsync
-                                 return arr|> Some 
-                                 }
-                         
+            let! events =
+                async {
+                    let! arr =
+                        stream.ReadAllAsync()
+                        |> AsyncSeq.ofAsyncEnum
+                        |> AsyncSeq.bufferByCountAndTime 1 (int pause.TotalMilliseconds)
+                        |> AsyncSeq.take 1
+                        |> AsyncSeq.toArrayAsync
 
-            let delay = async {
-                do! Async.Sleep pause
-                return Some [||]
-            }
+                    return arr.[0]
+                }
 
-            let! events =  seq {subs; delay} |> Async.Choice
 
-            let result = events |> Option.bind(fun arr -> arr |> Array.tryHead)
+            let result = events |> Array.tryHead
 
             do! userConnection1.StopAsync()
             do! userConnection2.StopAsync()
-            
+
             test
                 <@ match result with
-                   | Some m -> m.Text = message && Guid.Parse m.Group = group && m.User.Id = user1Id
+                   | Some m ->
+                       m.Text = message
+                       && Guid.Parse m.Group = group
+                       && m.User.Id = user1Id
                    | None -> false @>
         }
--- IntegrationTests/EventsTests.fs
+++ IntegrationTests/EventsTests.fs
@@ -20,7 +20,18 @@ type EventsTests(fixture: WebApplicationFactory<Program>) =
     let server = fixture.Server
     let apiClient = fixture.CreateClient()
     let cardsId = "66920B8F-3962-46FE-A2C1-434134B7F0FD"
-    let pause = TimeSpan.FromSeconds 5
+    let pause = TimeSpan.FromSeconds 10.0
+
+    let validateGrowingEventOrdering arr =
+        arr
+        |> Array.pairwise
+        |> Array.fold (fun state (a, b) -> state && a < b) true
+
+    let compareArraysByExistingElements (arr1: string array) (arr2: string array) =
+        let set1 = arr1 |> Set.ofArray
+        let set2 = arr2 |> Set.ofArray
+        set1 = set2
+
 
     [<Fact>]
     let ``The participant was added when SignalR connection has been the establishment`` () =
@@ -32,25 +43,21 @@ type EventsTests(fixture: WebApplicationFactory<Program>) =
 
             let! session = Helper.createSession apiClient user.Token "Session"
 
-            let! subscription =
-                connection.StreamAsChannelAsync<_>("Session", session.Id, session.Version)
+            let! subscription = connection.StreamAsChannelAsync<_>("Session", session.Id, session.Version)
 
             let! updatedSession = Helper.getSession apiClient user.Token session.Id
-            let subs = async {
-                                 let! arr = subscription.ReadAllAsync()
-                                             |> AsyncSeq.ofAsyncEnum
-                                             |> AsyncSeq.take(1)
-                                             |> AsyncSeq.toArrayAsync
-                                 return arr |> Some 
-                                 }
-                         
 
-            let delay = async {
-                do! Async.Sleep pause
-                return Some [||]
-            }
+            let! events =
+                async {
+                    let! arr =
+                        subscription.ReadAllAsync()
+                        |> AsyncSeq.ofAsyncEnum
+                        |> AsyncSeq.bufferByCountAndTime 1 (int pause.TotalMilliseconds)
+                        |> AsyncSeq.take (1)
+                        |> AsyncSeq.toArrayAsync
 
-            let! _ =  seq {subs; delay} |> Async.Choice
+                    return arr.[0]
+                }
 
             do! connection.StopAsync()
 
@@ -59,42 +66,53 @@ type EventsTests(fixture: WebApplicationFactory<Program>) =
 
 
     [<Fact>]
-    let ``The participant was removed when SignalR connection has been the closed`` () =
+    let ``The participant was removed when connection has been the closed`` () =
         task {
             let! user = Helper.login apiClient "test"
+            let! user2 = Helper.login apiClient "test"
             let! connection = Helper.createEventsConnection server user.Token
+            let! connection2 = Helper.createEventsConnection server user2.Token
 
             let! session = Helper.createSession apiClient user.Token "Session"
 
-            let! subscription =
-                connection.StreamAsChannelAsync<_>("Session", session.Id, session.Version)
+            let! subscription = connection.StreamAsChannelAsync<_>("Session", session.Id, session.Version)
 
-            let subs = async {
-                                 let! arr = subscription.ReadAllAsync()
-                                             |> AsyncSeq.ofAsyncEnum
-                                             |> AsyncSeq.take(1)
-                                             |> AsyncSeq.toArrayAsync
-                                 return arr |> Some 
-                                 }
-                         
+            let! subscription2 = connection2.StreamAsChannelAsync<_>("Session", session.Id, session.Version)
 
-            let delay = async {
-                do! Async.Sleep pause
-                return Some [||]
-            }
+            let! _ =
+                async {
+                    let! arr =
+                        subscription.ReadAllAsync()
+                        |> AsyncSeq.ofAsyncEnum
+                        |> AsyncSeq.bufferByCountAndTime 2 (int pause.TotalMilliseconds)
+                        |> AsyncSeq.take (1)
+                        |> AsyncSeq.toArrayAsync
 
-            let! _ =  seq {subs; delay} |> Async.Choice
+                    return arr.[0]
+                }
+
             let! updatedSession = Helper.getSession apiClient user.Token session.Id
 
-            test <@ updatedSession.Participants.Length = 1 @>
+            test <@ updatedSession.Participants.Length = 2 @>
 
-            do! connection.StopAsync()
+            do! connection2.StopAsync()
 
-            do! Task.Delay(TimeSpan.FromSeconds 1.)
+            let! _ =
+                async {
+                    let! arr =
+                        subscription.ReadAllAsync()
+                        |> AsyncSeq.ofAsyncEnum
+                        |> AsyncSeq.bufferByCountAndTime 1 (int pause.TotalMilliseconds)
+                        |> AsyncSeq.take (1)
+                        |> AsyncSeq.toArrayAsync
+
+                    return arr.[0]
+                }
 
             let! finallySession = Helper.getSession apiClient user.Token session.Id
+            do! connection.StopAsync()
 
-            test <@ finallySession.Participants.Length = 0 @>
+            test <@ finallySession.Participants.Length = 1 @>
         }
 
     [<Fact>]
@@ -109,35 +127,48 @@ type EventsTests(fixture: WebApplicationFactory<Program>) =
             let! session = Helper.createSession apiClient user.Token "Session"
 
             //action 3 "ParticipantAdded"
-            let! subscription =
-                connection.StreamAsChannelAsync<EventsDeliveryHub.Event>("Session", session.Id, 0)
+            let! subscription = connection.StreamAsChannelAsync<EventsDeliveryHub.Event>("Session", session.Id, 0)
 
             //action 3 StoryAdded
-            let! ses = Helper.addStoryToSession apiClient user.Token session { CreateStory.Title = "Story 1"; CardsId = cardsId; CustomCards = [|"1"; "2"|] }
+            let! ses =
+                Helper.addStoryToSession
+                    apiClient
+                    user.Token
+                    session
+                    { CreateStory.Title = "Story 1"
+                      CardsId = cardsId
+                      CustomCards = [| "1"; "2" |] }
 
             let storyId = ses.Stories.[0]
 
             //action 4 ActiveStorySet
             do! Helper.setActiveStory apiClient user.Token session.Id storyId
 
-            let subs = async {
-                                 let! arr = subscription.ReadAllAsync()
-                                             |> AsyncSeq.ofAsyncEnum
-                                             |> AsyncSeq.take(4)
-                                             |> AsyncSeq.toArrayAsync
-                                 return arr |> Array.map(fun e -> (e.Order, e.Type)) |> Some 
-                                 }
-                         
+            let! events =
+                async {
+                    let! arr =
+                        subscription.ReadAllAsync()
+                        |> AsyncSeq.ofAsyncEnum
+                        |> AsyncSeq.bufferByCountAndTime 4 (int pause.TotalMilliseconds)
+                        |> AsyncSeq.take (1)
+                        |> AsyncSeq.toArrayAsync
 
-            let delay = async {
-                do! Async.Sleep pause
-                return Some [||]
-            }
+                    return arr.[0] |> Array.map (fun e -> (e.Order, e.Type))
+                }
 
-            let! events =  seq {subs; delay} |> Async.Choice
             let! s = Helper.getSession apiClient user.Token session.Id
 
-            test <@ events = Some [|1,"Started"; 2,"ParticipantAdded"; 3,"StoryAdded"; 4,"ActiveStorySet"; |] || events = Some [|1,"Started";  2,"StoryAdded"; 3,"ParticipantAdded"; 4,"ActiveStorySet"; |]  @>
+            test <@ events.Length > 0 @>
+            test <@ validateGrowingEventOrdering (events |> Array.map (fst)) @>
+
+            test
+                <@ compareArraysByExistingElements
+                    (events |> Array.map (snd))
+                    [| "Started"
+                       "StoryAdded"
+                       "ParticipantAdded"
+                       "ActiveStorySet" |] @>
+
             test <@ s.Version = 4 @>
         }
 
@@ -154,35 +185,49 @@ type EventsTests(fixture: WebApplicationFactory<Program>) =
             //action 1 Started
             let! session = Helper.createSession apiClient user.Token "Session"
 
-           //action 2 "ParticipantAdded"
+            //action 2 "ParticipantAdded"
             let! subscription =
                 connection.StreamAsChannelAsync<EventsDeliveryHub.Event>("Session", session.Id, session.Version)
 
             //action 3 StoryAdded
-            let! ses = Helper.addStoryToSession apiClient user.Token session { CreateStory.Title = "Story 1"; CardsId = cardsId; CustomCards = [|"1"; "2"|] }
+            let! ses =
+                Helper.addStoryToSession
+                    apiClient
+                    user.Token
+                    session
+                    { CreateStory.Title = "Story 1"
+                      CardsId = cardsId
+                      CustomCards = [| "1"; "2" |] }
 
             let storyId = ses.Stories.[0]
 
             //action 4 ActiveStorySet
             let! s = Helper.setActiveStory apiClient user.Token session.Id storyId
-            let subs = async {
-                                 let! arr = subscription.ReadAllAsync()
-                                             |> AsyncSeq.ofAsyncEnum
-                                             |> AsyncSeq.take(3)
-                                             |> AsyncSeq.toArrayAsync
-                                 return arr |> Array.map(fun e -> (e.Order, e.Type)) |> Some 
-                                 }
-             
 
-            let delay = async {
-                do! Async.Sleep pause
-                return Some [||]
-            }
+            let! events =
+                async {
+                    let! arr =
+                        subscription.ReadAllAsync()
+                        |> AsyncSeq.ofAsyncEnum
+                        |> AsyncSeq.bufferByCountAndTime 3 (int pause.TotalMilliseconds)
+                        |> AsyncSeq.take (1)
+                        |> AsyncSeq.toArrayAsync
 
-            let! events =  seq {subs; delay} |> Async.Choice
+                    return arr.[0] |> Array.map (fun e -> (e.Order, e.Type))
+                }
+
             let! s = Helper.getSession apiClient user.Token session.Id
 
-            test <@ events = Some [|2,"ParticipantAdded"; 3,"StoryAdded"; 4,"ActiveStorySet"|] || events = Some [|2,"StoryAdded"; 3,"ParticipantAdded"; 4,"ActiveStorySet"|] @>
+            test <@ events.Length > 0 @>
+            test <@ validateGrowingEventOrdering (events |> Array.map (fst)) @>
+
+            test
+                <@ compareArraysByExistingElements
+                    (events |> Array.map (snd))
+                    [| "StoryAdded"
+                       "ParticipantAdded"
+                       "ActiveStorySet" |] @>
+
             test <@ s.Version = 4 @>
         }
 
@@ -197,14 +242,28 @@ type EventsTests(fixture: WebApplicationFactory<Program>) =
             let! session = Helper.createSession apiClient user.Token "Session"
 
             //action1 StoryConfigured
-            let! s = Helper.addStoryToSession apiClient user.Token session { CreateStory.Title = "Story 1"; CardsId = cardsId; CustomCards = [||] }
-            let! ses = Helper.addStoryToSession apiClient user.Token s { CreateStory.Title = "Story 2"; CardsId = cardsId; CustomCards = [||] }
+            let! s =
+                Helper.addStoryToSession
+                    apiClient
+                    user.Token
+                    session
+                    { CreateStory.Title = "Story 1"
+                      CardsId = cardsId
+                      CustomCards = [||] }
+
+            let! ses =
+                Helper.addStoryToSession
+                    apiClient
+                    user.Token
+                    s
+                    { CreateStory.Title = "Story 2"
+                      CardsId = cardsId
+                      CustomCards = [||] }
 
             let storyId = ses.Stories.[1]
             let anotherStoryId = ses.Stories.[0]
 
-            let! subscription =
-                connection.StreamAsChannelAsync<EventsDeliveryHub.Event>("Story", storyId, 0)
+            let! subscription = connection.StreamAsChannelAsync<EventsDeliveryHub.Event>("Story", storyId, 0)
 
 
             //action2  ActiveSet
@@ -213,15 +272,18 @@ type EventsTests(fixture: WebApplicationFactory<Program>) =
             //action3 Voted
             let! _ = Helper.vote apiClient user.Token storyId "XXS"
 
-            let groups = seq {(session.Groups.[0].Id, (session.Participants |> Array.map(fun p -> p.Id))) } |> dict |> Dictionary
+            let groups =
+                seq { (session.Groups.[0].Id, (session.Participants |> Array.map (fun p -> p.Id))) }
+                |> dict
+                |> Dictionary
 
             //action4 StoryClosed
-            let! _ = Helper.closeStory apiClient user.Token storyId {Groups = groups}
+            let! _ = Helper.closeStory apiClient user.Token storyId { Groups = groups }
 
             //action5  Paused
             do! Helper.setActiveStory apiClient user.Token session.Id anotherStoryId
             let! _ = Helper.vote apiClient user.Token anotherStoryId "XXS"
-            let! _ = Helper.closeStory apiClient user.Token anotherStoryId {Groups = groups}
+            let! _ = Helper.closeStory apiClient user.Token anotherStoryId { Groups = groups }
 
             //action6  ActiveSet
             do! Helper.setActiveStory apiClient user.Token session.Id storyId
@@ -234,66 +296,87 @@ type EventsTests(fixture: WebApplicationFactory<Program>) =
 
             let! s = Helper.getStory apiClient user.Token storyId
 
-            let subs = async {
-                                 let! arr = subscription.ReadAllAsync()
-                                             |> AsyncSeq.ofAsyncEnum
-                                             |> AsyncSeq.take(8)
-                                             |> AsyncSeq.toArrayAsync
-                                 return arr |> Array.map(fun e -> (e.Order, e.Type)) |> Some 
-                                 }
- 
+            let! events =
+                async {
+                    let! arr =
+                        subscription.ReadAllAsync()
+                        |> AsyncSeq.ofAsyncEnum
+                        |> AsyncSeq.bufferByCountAndTime 8 (int pause.TotalMilliseconds)
+                        |> AsyncSeq.take (1)
+                        |> AsyncSeq.toArrayAsync
 
-            let delay = async {
-                do! Async.Sleep pause
-                return Some [||]
-            }
-
-            let! events =  seq {subs; delay} |> Async.Choice
+                    return arr.[0] |> Array.map (fun e -> (e.Order, e.Type))
+                }
 
             do! connection.StopAsync()
 
-            test <@ events = Some [|1,"StoryConfigured"; 2,"ActiveSet"; 3,"Voted"; 4,"StoryClosed"; 5,"Paused"; 6,"ActiveSet"; 7,"Cleared"; 8,"Paused" |] @>
+            test
+                <@ events = [| 1, "StoryConfigured"
+                               2, "ActiveSet"
+                               3, "Voted"
+                               4, "StoryClosed"
+                               5, "Paused"
+                               6, "ActiveSet"
+                               7, "Cleared"
+                               8, "Paused" |] @>
+
             test <@ s.Version = 8 @>
         }
 
     [<Fact>]
-    let ``The workflow add group, move Participant, remove group works fine``() =
+    let ``The workflow add group, move Participant, remove group works fine`` () =
         task {
 
             let! user = Helper.login apiClient "test"
             let tokenHandler = JwtSecurityTokenHandler()
             let token = tokenHandler.ReadJwtToken(user.Token)
-            let userId =  token.Claims |> Seq.filter(fun c -> c.Type = "nameid") |> Seq.tryHead |> Option.map(fun c -> c.Value |> Guid.Parse ) |> Option.defaultValue Guid.Empty
+
+            let userId =
+                token.Claims
+                |> Seq.filter (fun c -> c.Type = "nameid")
+                |> Seq.tryHead
+                |> Option.map (fun c -> c.Value |> Guid.Parse)
+                |> Option.defaultValue Guid.Empty
 
             let! connection = Helper.createEventsConnection server user.Token
             let! session = Helper.createSession apiClient user.Token "Session"
 
-            let! subscription =
-                connection.StreamAsChannelAsync<EventsDeliveryHub.Event>("Session", session.Id, 0)
+            let! subscription = connection.StreamAsChannelAsync<EventsDeliveryHub.Event>("Session", session.Id, 0)
 
             let! sessionWithGroup = Helper.addGroup apiClient user.Token session.Id "group"
-            let group = sessionWithGroup.Groups |> Array.find (fun g -> g.Id <> session.DefaultGroupId)
+
+            let group =
+                sessionWithGroup.Groups
+                |> Array.find (fun g -> g.Id <> session.DefaultGroupId)
+
             let! _ = Helper.moveParticipantToGroup apiClient user.Token session.Id userId group.Id
             let! s = Helper.removeGroup apiClient user.Token session.Id group.Id
 
-            let subs = async {
-                                 let! arr = subscription.ReadAllAsync()
-                                             |> AsyncSeq.ofAsyncEnum
-                                             |> AsyncSeq.take(5)
-                                             |> AsyncSeq.toArrayAsync
-                                 return arr |> Array.map(fun e -> (e.Order, e.Type)) |> Some 
-                                 }
- 
+            let! events =
+                async {
+                    let! arr =
+                        subscription.ReadAllAsync()
+                        |> AsyncSeq.ofAsyncEnum
+                        |> AsyncSeq.bufferByCountAndTime 5 (int pause.TotalMilliseconds)
+                        |> AsyncSeq.take (1)
+                        |> AsyncSeq.toArrayAsync
 
-            let delay = async {
-                do! Async.Sleep pause
-                return Some [||]
-            }
+                    return arr.[0] |> Array.map (fun e -> (e.Order, e.Type))
+                }
 
-            let! events =  seq {subs; delay} |> Async.Choice
             do! connection.StopAsync()
 
-            test <@ events = Some [|1,"Started"; 2,"ParticipantAdded"; 3,"GroupAdded"; 4,"ParticipantMovedToGroup"; 5,"GroupRemoved" |] || events = Some [|1,"Started"; 2,"GroupAdded"; 3,"ParticipantAdded"; 4,"ParticipantMovedToGroup"; 5,"GroupRemoved" |] @>
-            test <@ s.Version = 5 @>
+            test <@ events.Length > 0 @>
+            test <@ validateGrowingEventOrdering (events |> Array.map (fst)) @>
 
-            }
+            test
+                <@ compareArraysByExistingElements
+                    (events |> Array.map (snd))
+                    [| "Started"
+                       "ParticipantAdded"
+                       "GroupAdded"
+                       "ParticipantMovedToGroup"
+                       "GroupRemoved" |] @>
+
+            test <@ s.Version = 5 @>
+        }
